0741b4eee7d102e9f82f5451f4715f25318d46db
mnebot86 <miguelnbt@gmail.com>
2024-11-13 23:24:51 -0500

    changed port

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
+		projectId: '3gt71s',
 		setupNodeEvents(on, config) {
 			config.baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
