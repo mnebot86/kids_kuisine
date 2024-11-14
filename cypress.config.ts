4eb3df0a65a3a1dfa867c3d1c685f0f5ec37de8c
mnebot86 <miguelnbt@gmail.com>
2024-11-13 23:31:23 -0500

    conflict

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			config.baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+
+			return config;
 		},
 	},
 });
