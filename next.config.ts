6583407b9b533b460965ec61563c17892bf64146
mnebot86 <miguelnbt@gmail.com>
2024-11-14 22:41:09 -0500

    Add export to next config option

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	trailingSlash: true,
-	output: 'standalone',
+	output: 'export',
 };
 
 export default nextConfig;
