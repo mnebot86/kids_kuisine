ba3d9677be1c844f580092da9ff4ed6b10993383
mnebot86 <miguelnbt@gmail.com>
2024-11-14 23:20:06 -0500

    add baseUrl to crypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		projectId: '3gt71s',
+		baseUrl: 'http://localhost:3005',
 		setupNodeEvents(on, config) {
 			config.baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
