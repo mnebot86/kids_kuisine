5d696881c6c97c054dd8cecdc9c62550197b821e
mnebot86 <miguelnbt@gmail.com>
2024-11-14 23:25:31 -0500

    add baseUrl to crypress config

--- cypress/e2e/home.spec.cy.ts
+++ cypress/e2e/home.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Homepage', () => {
 	it('should display the main heading', () => {
-		cy.visit('http://localhost:3000');
+		cy.visit('http://localhost:3005');
 		cy.get('h1').contains('New Project');
 	});
 });
