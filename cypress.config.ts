85182f282bf563487da18c1ce4b3bff99df590ed
mnebot86 <miguelnbt@gmail.com>
2024-11-28 01:29:04 -0500

    Move git template and updated husky commands

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	e2e: {
 		projectId: '3gt71s',
 		setupNodeEvents(on, config) {
-			config.baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+			config.baseUrl = process.env.BASE_URL || 'http://localhost:3005';
 
 			return config;
 		},
