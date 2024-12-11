d73048b1ef01a3aaafc8e5a1a4906d7c4c6989e2
CWells1225 <86331991+CWells1225@users.noreply.github.com>
2024-12-11 09:51:37 -0500

    Feature/hero (#8)
    * hero
    
    * hero component done
    
    * hero component modified styling
    
    * updated changes to hero component, pages, and cypress file
    
    * updated layout and pages

---
+++ app/component/home/Hero.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
+
+const Hero: React.FC = () => (
+	<Container
+		maxWidth="xl"
+		sx={{
+			backgroundImage:
+				'url(https://cdn.create.vista.com/api/media/small/5755752/stock-photo-healthy-eating)',
+			backgroundSize: 'cover',
+			backgroundPosition: 'center',
+			height: { xs: '25vh', md: '50vh', lg: '75vh' },
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+		}}
+	>
+		<Box
+			sx={{
+				textAlign: 'center',
+				backgroundColor: '#A2C428',
+				width: '700px',
+				borderRadius: '10px',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				padding: '20px',
+				gap: 2,
+			}}
+		>
+			<Typography
+				variant="h1"
+				sx={{
+					color: '#F8F0E6',
+					fontSize: { xs: '20px', md: '36px', lg: '45px' },
+					fontFamily: 'Montserrat',
+					fontWeight: 'bold',
+					textAlign: 'center',
+					textTransform: 'uppercase',
+				}}
+			>
+				Made for Moms for Kids
+			</Typography>
+			<Typography
+				variant="h3"
+				sx={{
+					color: '#F8F0E6',
+					fontSize: { xs: '14px', md: '20px', lg: '24px' },
+					fontFamily: 'Montserrat',
+					textAlign: 'center',
+					maxWidth: '566px',
+				}}
+			>
+				Ending child hunger and promoting better nutrition can create a
+				lasting impact on communities and future generations.
+			</Typography>
+			<Button
+				variant="contained"
+				href="/menu"
+				sx={{
+					color: '#F8F0E6',
+					backgroundColor: '#7DDCFA',
+					fontSize: { xs: '14px', md: '20px', lg: '24px' },
+					fontFamily: 'Montserrat',
+					fontWeight: 'bold',
+					textAlign: 'center',
+				}}
+			>
+				VIEW MENU
+			</Button>
+		</Box>
+	</Container>
+);
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
+
+import Hero from './component/home/Hero';
 
 const HomePage = () => (
 	<Container
 		maxWidth={false}
 		sx={{
-			height: '100vh',
 			display: 'flex',
 			alignItems: 'center',
 			justifyContent: 'center',
 			textAlign: 'center',
-			bgcolor: 'primary.main',
-			color: 'white',
 		}}
 	>
-		<Typography variant="h2" component="h1" sx={{ fontSize: '4rem' }}>
-			New Project
-		</Typography>
+		<Hero />
 	</Container>
 );
 
