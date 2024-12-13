6cadceb99d1ed3af10bf16fdc1fff84885f657ad
CWells1225 <86331991+CWells1225@users.noreply.github.com>
2024-12-13 10:53:00 -0500

    Feature/best sellers (#9)
    * merged hero branch to dev branch
    
    * updates to styling best-sellers comp
    
    * best sellers component done

---
+++ app/component/home/best-sellers.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+	Button,
+	Card,
+	CardContent,
+	CardMedia,
+	Container,
+	Typography,
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
+
+interface BestSellersProps {
+	items: Item[];
+}
+
+interface Item {
+	id: number;
+	name: string;
+	image: string;
+	description: string;
+}
+
+const BestSellers: React.FC<BestSellersProps> = ({ items }) => (
+	<Container maxWidth="xl">
+		<Typography
+			variant="h3"
+			align="center"
+			gutterBottom
+			sx={{
+				color: '#A2C428',
+				fontFamily: 'Montserrat',
+				fontSize: '35px',
+				textTransform: 'uppercase',
+				fontWeight: 'bold',
+				padding: '20px',
+			}}
+		>
+			Best Sellers
+		</Typography>
+		<Grid container spacing={4}>
+			{items.map((item) => (
+				<Grid key={item.id} size={{ xs: 12, sm: 6, md: 4 }}>
+					<Card>
+						<CardMedia
+							component="img"
+							height="200"
+							image={item.image}
+						/>
+						<CardContent
+							sx={{
+								backgroundColor: '#FFFDFD',
+							}}
+						>
+							<Typography
+								variant="h6"
+								gutterBottom
+								sx={{
+									color: '#A2C428',
+									fontFamily: 'Montserrat',
+									fontSize: '12px',
+									fontStyle: 'bold',
+								}}
+							>
+								{item.name}
+							</Typography>
+							<Typography
+								variant="body2"
+								sx={{
+									color: '#A2C428',
+									fontFamily: 'Montserrat',
+									fontSize: '12px',
+									fontStyle: 'bold',
+								}}
+							>
+								{item.description}
+							</Typography>
+							<Button variant="contained" fullWidth>
+								Add to Cart
+							</Button>
+						</CardContent>
+					</Card>
+				</Grid>
+			))}
+		</Grid>
+	</Container>
+);
+
+export default BestSellers;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,52 @@
 import { Container } from '@mui/material';
 
+import BestSellers from './component/home/best-sellers';
 import Hero from './component/home/Hero';
+
+interface Item {
+	id: number;
+	name: string;
+	image: string;
+	description: string;
+}
+
+const items: Item[] = [
+	{
+		id: 1,
+		name: 'Lasagna',
+		image: 'https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-12-scaled.jpg',
+		description: 'Cheesy lasagna',
+	},
+	{
+		id: 2,
+		name: 'Chicken Caesar Salad',
+		image: 'https://heartbeetkitchen.com/foodblog/wp-content/uploads/2022/06/ultimate-grilled-chicken-caesar-salad.jpg',
+		description:
+			'Chicken, avocadoes, vegetables drizzled in caesar dressing',
+	},
+
+	{
+		id: 3,
+		name: 'Turkey sandwich',
+		image: 'https://www.theoriginaldish.com/wp-content/uploads/2023/01/Smoked-Turkey-Sandwich-2-scaled.jpg',
+		description: 'Turkey sandwich with cheese and vegetables',
+	},
+];
 
 const HomePage = () => (
 	<Container
 		maxWidth={false}
 		sx={{
 			display: 'flex',
+			flexDirection: 'column',
 			alignItems: 'center',
 			justifyContent: 'center',
 			textAlign: 'center',
+			backgroundColor: 'background.paper',
 		}}
 	>
 		<Hero />
+		<BestSellers items={items} />
 	</Container>
 );
 
